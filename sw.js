01211435390535392e4a90b50b86ae459c196169
jjui <nevermindreve@gmail.com>
2026-03-19 10:54:02 +0900

    sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'gyaru-mind-v1';
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll([
+                './',
+                './index.html'
+            ]);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        fetch(e.request).catch(() => {
+            return caches.match(e.request);
+        })
+    );
+});
